66ebd136ebbe2707ce4e40769a5630415fc78e4e
AndreLuvetama <andreluares30@gmail.com>
2021-03-04 21:23:20 -0300

    hr-workers implementation

---
+++ hr-worker/src/main/java/com/microservices/worker/repositories/WorkerRepository.java
@@ -0,0 +1,10 @@
+package com.microservices.worker.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.microservices.worker.entities.Worker;
+
+public interface WorkerRepository extends JpaRepository<Worker , Long>{
+	
+
+}
