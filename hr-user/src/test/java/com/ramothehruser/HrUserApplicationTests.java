6199cfe81af3b96367cfbd0824a2d868853daa10
AndreLuvetama <andreluares30@gmail.com>
2021-03-16 22:24:10 -0300

    Seed database

---
+++ hr-user/src/test/java/com/ramothehruser/HrUserApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ramothehruser;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HrUserApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
